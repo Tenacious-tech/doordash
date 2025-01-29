ae8550317d356dbb05baa8f8cd3c007350a43f7c
Tenacious-tech <wordpress.techies2020@gmail.com>
2025-01-29 11:58:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import deliveryRoute from './routes/deliveryRoute.js';
-import { connectDB } from './config/db.js';
+// import { connectDB } from './config/db.js';
+import { login } from './controller/loginController.js';
+import authMiddleware from './middlewares/authMiddleware.js';
 
 dotenv.config();
 
 const app=express();
 
-connectDB();
+// connectDB();
 app.use(express.json());
-app.use('/',deliveryRoute);
+app.post('/login',login);
+app.use('/',authMiddleware,deliveryRoute);
 
 app.listen(3000,()=>{
     console.log("hogaya connect");
